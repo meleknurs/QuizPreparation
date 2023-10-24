d20c96521093a2f3b94993e13dfe6d8bbe768649
Viktor Khrapal <145751325+KingInspectorVector@users.noreply.github.com>
2023-10-24 15:57:52 +0200

    JS try to do second way

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+/*const sectionField = document.getElementById("section");
+
+function QuestionList(question, answer1, answer2, answer3, answer4) {
+    this.question = question;
+    this.answer1 = answer1;
+    this.answer2 = answer2;
+    this.answer3 = answer3;
+    this.answer4 = answer4;
+}
+
+const question = [
+
+]*/
+
 class QuestionList {
     constructor() {
         this.list = [];
